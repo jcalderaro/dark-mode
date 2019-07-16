1ed058118002d03d925b0ca8eb83c169408265aa
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-07-16 15:31:50 -0700

    Peer Program Breakout Room Complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import useLocalStorage from "./useLocalStorage";
+import {useEffect} from 'react';
+
+const useDarkMode = () => {
+    const [storedValue, setStoredValue] = useLocalStorage("toggle");
+
+    useEffect(() => {
+        storedValue 
+            ? document.body.classList.add("dark-mode") 
+            : document.body.classList.remove("dark-mode")
+        }, 
+            [storedValue]);
+
+return [storedValue, setStoredValue];
+
+};
+
+export default useDarkMode;
