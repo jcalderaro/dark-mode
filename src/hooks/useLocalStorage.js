7a34d604b7fd9ccac717a1d3ea9710469a240d10
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-10-02 13:44:12 -0700

    MVP 2 Greentext

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -62,22 +62,22 @@ export default useLocalStorage;
 
 /* Explainiing The Code
 
-Line 45:
-Line 46:
-Line 47:
-Line 48:
-Line 49:
-Line 50:
-Line 51:
-Line 52:
-Line 53:
-Line 54:
-Line 55:
-Line 56:
-Line 57:
-Line 58:
-Line 59:
-Line 60:
-Line 61:
+Line 45: We are IMPORTING our, UseState, from React Natively
+Line 46: - Empty Space
+Line 47: We create our constant, custom hook, useLocalStorage, that takes in a key + initial value pair. 
+Line 48: ?
+Line 49: ?
+Line 50: ?
+Line 51: - Notation
+Line 52: - Empty Space
+Line 53: We create our constant, second function within our hook, called SetValue
+Line 54: ?
+Line 55: ?
+Line 56: - Notation
+Line 57: - Empty Space
+Line 58: We are returning, rendering, the StoredValue, and SetValue
+Line 59: - Notation
+Line 60: - Empty Space
+Line 61: We are now EXPORTING our custom hook, UseLocalStorage, so that it can be referenced elsewhere
 
 */
