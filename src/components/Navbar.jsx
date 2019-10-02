7200355ea8bf747b85b25664ec44aeb3ed66890e
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-10-02 13:31:48 -0700

    Defaults

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
-import React, { useState } from 'react';
+/*
+
+## STEP 3 - Using the hook in a component
+
+Now that we have composed our different pieces of stateful logic, let's use it in our component!
+
+- Import the dark mode hook into the `NavBar` component
+
+- Looking at this component, we see that we are controlling the toggle with some state. 
+
+The state hook here returns a `darkMode` value, and a `setDarkMode` function. 
+
+- Isn't that exactly what our `useDarkMode` hook returns? Replace the state hook with our hook, 
+
+Click the toggle to verify
+
+*/
+
 import useDarkMode from '../hooks/useDarkMode.js'
+
+import React, { useState } from 'react';
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
